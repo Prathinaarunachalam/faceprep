6b1ef30001c34de75b25eacd673da2c4a1ff14b7
Prathinaarunachalam <12183.prathinaa@gmail.com>
2023-06-29 18:32:30 +0530

    updated

--- faceprep 16.c
+++ faceprep 16.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int a,r,c;
+    scanf("%d",&a);
+    r = (a-1)/3;
+    c = (a-1)%3;
+    printf("%d %d",r,c);
+    return 0;
+}
 
